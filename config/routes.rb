6b402d92d9e3456a2416ee838342a1dd6f9cbad7
Ernesto Nakamura <ernesto@naka.eti.br>
2013-09-12 11:53:34 -0300

    Set root route do rodovias#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rodovias::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'rodovias#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
