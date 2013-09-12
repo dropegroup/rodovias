ad211d4c14c6784e830fa3c990326a218184ca89
Ernesto Nakamura <ernesto@naka.eti.br>
2013-09-11 22:43:07 -0300

    rails new rodovias

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rodovias::Application.config.secret_key_base = 'f317bff5c00b627064ca0f7ebd47bb71ba8a6e1baf5d0bf0475891d5c395ca1f699d5dbf9385ef65a8752a8c65b0cfd13e46fd588a71be90998483bc9a17f8bd'
